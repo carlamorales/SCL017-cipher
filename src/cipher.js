68f748ea1d25c3dbd472ee667821a7f7b052229f
carlamorales <carlamoralespacheco@gmail.com>
2021-05-13 11:56:13 -0400

    se agregan/modifican eventos de DOM y método cipher.encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
 const cipher = {
-  // ...
+  encode: (string, offset) => {
+    let newString = "";
+    for (let i = 0; i < string.length; i++) {
+      let asciiNum = string.charCodeAt(i);
+      if (asciiNum >= 65 && asciiNum <= 90) {
+        asciiNum = (((asciiNum - 65 + offset) % 26) + 65);
+      } else if (asciiNum >= 97 && asciiNum <= 122) {
+        asciiNum = (((asciiNum - 97 + offset) % 26) + 97);
+      }
+      newString += String.fromCharCode(asciiNum);
+    }
+    return newString;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+document.getElementById("toEncodePage").addEventListener("click", toEncodePage)
+function toEncodePage() {
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("encodePage").style.display = "block";
+}
+
+document.getElementById("toDecodePage").addEventListener("click", toDecodePage)
+function toDecodePage() {
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("decodePage").style.display = "block";
+}
+
+document.getElementById("toAboutPage").addEventListener("click", toAboutPage)
+function toAboutPage() {
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("aboutPage").style.display = "block";
+}
+
+document.getElementById("toTheStart1").addEventListener("click", toTheStart1)
+function toTheStart1() {
+  document.getElementById("aboutPage").style.display = "none";
+  document.getElementById("homepage").style.display = "block";
+}
+
+document.getElementById("toTheStart2").addEventListener("click", toTheStart2)
+function toTheStart2() {
+  document.getElementById("encodePage").style.display = "none";
+  document.getElementById("homepage").style.display = "block";
+}
+
+document.getElementById("toTheStart3").addEventListener("click", toTheStart3)
+function toTheStart3() {
+  document.getElementById("decodePage").style.display = "none";
+  document.getElementById("homepage").style.display = "block";
+}
+
+document.getElementById("funEnc").addEventListener("click", funEnc)
+function funEnc() {
+  let string = document.getElementById("inTxt").value;
+  let offset = parseInt(document.getElementById("toTheRight").value);
+
+  document.getElementById("outTxt").value = cipher.encode(string, offset);
+}
