4afed19a674f7dd46a02855a898e43c6b63f7d02
carlamorales <carlamoralespacheco@gmail.com>
2021-05-14 15:14:36 -0400

    edición y actualización general

--- src/index.js
+++ src/index.js
@@ -2,46 +2,32 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-document.getElementById("toEncodePage").addEventListener("click", toEncodePage)
-function toEncodePage() {
+document.getElementById("toEncPage").addEventListener("click", function() {
   document.getElementById("homepage").style.display = "none";
-  document.getElementById("encodePage").style.display = "block";
-}
-
-document.getElementById("toDecodePage").addEventListener("click", toDecodePage)
-function toDecodePage() {
+  document.getElementById("encodePage").style.display ="block";
+});
+document.getElementById("toDecPage").addEventListener("click", function() {
   document.getElementById("homepage").style.display = "none";
   document.getElementById("decodePage").style.display = "block";
-}
-
-document.getElementById("toAboutPage").addEventListener("click", toAboutPage)
-function toAboutPage() {
+});
+document.getElementById("toAbtPage").addEventListener("click", function() {
   document.getElementById("homepage").style.display = "none";
   document.getElementById("aboutPage").style.display = "block";
-}
-
-document.getElementById("toTheStart1").addEventListener("click", toTheStart1)
-function toTheStart1() {
+});
+document.getElementById("toHPage1").addEventListener("click", function() {
   document.getElementById("aboutPage").style.display = "none";
   document.getElementById("homepage").style.display = "block";
-}
-
-document.getElementById("toTheStart2").addEventListener("click", toTheStart2)
-function toTheStart2() {
+});
+document.getElementById("toHPage2").addEventListener("click", function() {
   document.getElementById("encodePage").style.display = "none";
   document.getElementById("homepage").style.display = "block";
-}
-
-document.getElementById("toTheStart3").addEventListener("click", toTheStart3)
-function toTheStart3() {
+});
+document.getElementById("toHPage3").addEventListener("click", function() {
   document.getElementById("decodePage").style.display = "none";
   document.getElementById("homepage").style.display = "block";
-}
-
-document.getElementById("funEnc").addEventListener("click", funEnc)
-function funEnc() {
+});
+document.getElementById("encodeBtn").addEventListener("click", function() {
   let string = document.getElementById("inTxt").value;
   let offset = parseInt(document.getElementById("toTheRight").value);
-
   document.getElementById("outTxt").value = cipher.encode(string, offset);
-}
+});
